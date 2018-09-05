d3d54b46457f5a9f4c19a6de69c9dc63790f0448
Peter Hsiesh <derfushi@gmail.com>
2018-09-05 23:28:07 +0800

    program for bitcoin tracing project

---
+++ BQ.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+from google.cloud.bigquery.client import Client
+SERVICE_ACCOUNT_JSON = "file/Bitcoin-Tracing-472072344e9c.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = SERVICE_ACCOUNT_JSON
+client = Client()
+query = """
+#standardSQL
+SELECT
+  o.day,
+  COUNT(DISTINCT(o.output_key)) AS recipients
+FROM (
+  SELECT
+    TIMESTAMP_MILLIS((timestamp - MOD(timestamp,
+          86400000))) AS day,
+    output.output_pubkey_base58 AS output_key
+  FROM
+    `bigquery-public-data.bitcoin_blockchain.transactions`,
+    UNNEST(outputs) AS output ) AS o
+GROUP BY
+  day
+ORDER BY
+  day
+"""
+query_job = client.query(query)
+
+iterator = query_job.result(timeout=3000)
+rows = list(iterator)
+
+# Transform the rows into a nice pandas dataframe
+transactions = pd.DataFrame(data=[list(x.values()) for x in rows], columns=list(rows[0].keys()))
+
+# Look at the first 10 headlines
+print(transactions.tail(10))
