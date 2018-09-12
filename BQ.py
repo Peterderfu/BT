c121ef4d42f534bc4c2816aa288aac8d9fa20ac0
house <house@DESKTOP-J92RPHJ.localdomain>
2018-09-12 23:35:13 +0800

    gtet

--- BQ.py
+++ BQ.py
@@ -1,10 +1,11 @@
 import os
-import pandas as pd
+# import pandas as pd
 from google.cloud.bigquery.client import Client
+# from bq_helper import BigQueryHelper
 SERVICE_ACCOUNT_JSON = "file/Bitcoin-Tracing-472072344e9c.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = SERVICE_ACCOUNT_JSON
 client = Client()
-query = """ 
+query1 = """
 #standardSQL
 SELECT
   o.day,
@@ -22,13 +23,30 @@ GROUP BY
 ORDER BY
   day
 """
-query_job = client.query(query)
-
-iterator = query_job.result(timeout=3000)
-rows = list(iterator)
-
-# Transform the rows into a nice pandas dataframe
-transactions = pd.DataFrame(data=[list(x.values()) for x in rows], columns=list(rows[0].keys()))
-
-# Look at the first 10 headlines
-print(transactions.tail(10))
+# query_job = client.query(query)
+# 
+# iterator = query_job.result(timeout=3000)
+# rows = list(iterator)
+# 
+# # Transform the rows into a nice pandas dataframe
+# transactions = pd.DataFrame(data=[list(x.values()) for x in rows], columns=list(rows[0].keys()))
+# 
+# # Look at the first 10 headlines
+# print(transactions.tail(10))
+def list_field(dataset):
+'''
+list all the fields and sub-fields in dataset
+'''
+    hn_dataset_ref = client.dataset(dataset[1], project=dataset[0])
+    hn_dset = client.get_dataset(hn_dataset_ref)
+    for t in client.list_tables(hn_dset):
+        hn_full = client.get_table(hn_dset.table(t.table_id))
+        for f1 in hn_full.schema:
+            if (f1.fields):
+                for f2 in f1.fields:
+                    print("".join([t.table_id, ":",f1.name, "[",f1.field_type,"]",":",f2.name, "[",f2.field_type,"]"]))
+            else:
+                print("".join([t.table_id, ":",f1.name, "[",f1.field_type,"]"]))
+if __name__ == "__main__":
+    list_field(['bigquery-public-data','bitcoin_blockchain'])
+    
