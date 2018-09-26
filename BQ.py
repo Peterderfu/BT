4f4826a2f28e02c583054f37be21db7eb4a7b8db
house <house@DESKTOP-J92RPHJ.localdomain>
2018-09-26 22:44:19 +0800

    1

--- BQ.py
+++ BQ.py
@@ -1,40 +1,13 @@
 #https://www.kaggle.com/bigquery/bitcoin-blockchain
 import os
 import pandas as pd
-from google.cloud.bigquery.client import Client
+from google.cloud import bigquery
 from datetime import datetime,timezone
 # from bq_helper import BigQueryHelper
 SERVICE_ACCOUNT_JSON = "file/Bitcoin-Tracing-472072344e9c.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = SERVICE_ACCOUNT_JSON
-client = Client()
-query1 = """
-#standardSQL
-SELECT
-  o.day,
-  COUNT(DISTINCT(o.output_key)) AS recipients
-FROM (
-  SELECT
-    TIMESTAMP_MILLIS((timestamp - MOD(timestamp,
-          86400000))) AS day,
-    output.output_pubkey_base58 AS output_key
-  FROM
-    `bigquery-public-data.bitcoin_blockchain.transactions`,
-    UNNEST(outputs) AS output ) AS o
-GROUP BY
-  day
-ORDER BY
-  day
-"""
-# query_job = client.query(query)
-# 
-# iterator = query_job.result(timeout=3000)
-# rows = list(iterator)
-# 
-# # Transform the rows into a nice pandas dataframe
-# transactions = pd.DataFrame(data=[list(x.values()) for x in rows], columns=list(rows[0].keys()))
-# 
-# # Look at the first 10 headlines
-# print(transactions.tail(10))
+client = bigquery.Client()
+
 def list_field(dataset):
 #list all the fields and sub-fields in dataset
     hn_dataset_ref = client.dataset(dataset[1], project=dataset[0])
@@ -50,29 +23,48 @@ def list_field(dataset):
 
 
 if __name__ == "__main__":
+    job_config = bigquery.QueryJobConfig()
+#     job_config.dry_run = True
+#     job_config.use_query_cache = False
 #     list_field(['bigquery-public-data', 'bitcoin_blockchain'])
 #     exit
-    date_str = "2009-12-31"
-    dt = datetime.strptime(date_str, "%Y-%m-%d")
-    timestamp = int(dt.replace(tzinfo=timezone.utc).timestamp())*1000
+    date_start_str = "2009-1-30"
+    date_stop_str  = "2009-1-31"
+    dt = datetime.strptime(date_start_str, "%Y-%m-%d")
+    timestamp_start = int(dt.replace(tzinfo=timezone.utc).timestamp())*1000 # to millisecond
+    dt = datetime.strptime(date_stop_str, "%Y-%m-%d")
+    timestamp_stop  = int(dt.replace(tzinfo=timezone.utc).timestamp())*1000 # to millisecond
     
     query = """
     SELECT
-        timestamp,outputs
+        timestamp,o.output_pubkey_base58
     FROM
-        `bigquery-public-data.bitcoin_blockchain.transactions`
+        `bigquery-public-data.bitcoin_blockchain.transactions`,UNNEST(outputs) AS o
     WHERE
-        timestamp < """ + str(timestamp) + """
+        timestamp > """ + str(timestamp_start) + """ AND timestamp < """ + str(timestamp_stop) + """
     ORDER BY
        timestamp
+    LIMIT 1
     """
-    print("Performing query : ")
-    print(query)
-    query_job = client.query(query)
+    
+    print("Performing query :\n" + query)
+    query_job = client.query(query,job_config=job_config)
  
-    iterator = query_job.result(timeout=3000)
-    rows = list(iterator)
-    transactions = pd.DataFrame(data=[list(x.values()) for x in rows], columns=list(rows[0].keys()))
-    transactions.head(10)
-    assert(1==1)
+#     assert query_job.state == 'DONE'
+#     assert query_job.dry_run
+    
+    if (query_job.total_bytes_processed):
+        n = query_job.total_bytes_processed;
+    else:
+        n = 0
+    print("This query will process {} bytes.".format(n/(1024^3)))
+    
+    if (job_config.dry_run):
+        exit
+    else:
+        iterator = query_job.result(timeout=30)
+        rows = list(iterator)
+        transactions = pd.DataFrame(data=[list(x.values()) for x in rows], columns=list(rows[0].keys()))
+        print(transactions.head(10))
+        assert(1==1)
     
